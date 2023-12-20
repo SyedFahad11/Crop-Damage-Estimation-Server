f2f99848e6121aa7e0e6cdde98bef177ff7566ee
Anirudh Joshi <vuddawaranirudhjoshi03@gmail.com>
2023-12-20 22:47:05 +0530

    Improved ui

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,6 +23,7 @@ async function login(req, res) {
     const user = await User.findOne({ username });
 
     if (!user) {
+      console.log("User not found")
       return res.status(401).send('Invalid credentials');
     }
 
